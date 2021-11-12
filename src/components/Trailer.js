d92c9c269ccb552d5abbc8f0e9aa28916993dae3
ARMAN ALI <31398598+a-mal1k@users.noreply.github.com>
2021-11-12 13:34:39 +0530

    added movies api (incomplete)

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,38 +1,28 @@
 import $ from "jquery";
-import { useState,useEffect } from "react";
-const Trailer = () => {
+import { useState } from "react";
+const Trailer = (props) => {
   const [play, setPlay] = useState(false);
-
-  let image =
-    "https://www.pngkit.com/png/detail/94-948126_spider-man-flying-spiderman-png.png";
 
    if(play){
      $('.play').css('display','block')
    }else{
     $('.play').css('display','none')
    }
-useEffect(()=>{
-fetch("https://peaceful-forest-62260.herokuapp.com/")
-.then(res => res.json())
-.then(data => {
-  console.log(data.moviesData)
-})
-},[])
   return (
-    <div className="card" style={{ backgroundImage: `url(${image})` }}>
+    <div className="card" style={{ backgroundImage: `url(${props.image})` }}>
       <div className="inner-card">
-        <p className="date">5 Dec</p>
+        <p className="date">{props.date}</p>
         <div className="voting">
           <span>
-            <i className="fa fa-thumbs-up"></i>100%
+            <i className="fa fa-thumbs-up"></i>{props.votePer}
           </span>
-          <h6>55555 votes</h6>
+          <h6>{props.vote} votes</h6>
         </div>
       </div>
       <button className="btn" onClick={()=>setPlay(true)} onDoubleClick={()=>setPlay(false)}>
         <i className="fa fa-play"></i>
       </button>
-      <p className="movieName">Movie Name</p>
+      <p className="movieName">{props.movieName}</p>
     </div>
   );
 };
--- src/components/Trailers.js
+++ src/components/Trailers.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import Trailer from "./Trailer";
 const Trailers = () => {
+  var apiData = [
+    { date: "" },
+    { image: "" },
+    { vote: "" },
+    { votePer: "" },
+    { movieName: "" }
+  ];
+  const fetchData = () => {
+    fetch("https://peaceful-forest-62260.herokuapp.com/")
+      .then((res) => res.json())
+      .then((data) => {
+        Object.values(data.moviesData).map((t) => {
+          apiData.date.push(t.showDate);
+          apiData.image.push(t.EventImageUrl);
+          apiData.vote.push(t.csCount);
+          apiData.votePer.push(t.wtsPerc);
+          apiData.movieName.push(t.EventTitle);
+          return apiData;
+        });
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="trailers">
-      <Trailer />
-      <Trailer />
-      <Trailer />
-      <Trailer />
+      {apiData.map((data) => {
+        return (
+          <Trailer
+            date={data.date}
+            image={data.image}
+            vote={data.vote}
+            votePer={data.votePer}
+            movieName={data.movieName}
+          />
+        );
+      })}
     </div>
   );
 };
