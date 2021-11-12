084bad31339e9b542f40fb67f866bbc0ee7fa6ff
ARMAN ALI <31398598+a-mal1k@users.noreply.github.com>
2021-11-12 10:51:56 +0530

    added play div

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,15 +1,23 @@
 import $ from "jquery";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 const Trailer = () => {
   const [play, setPlay] = useState(false);
 
   let image =
     "https://www.pngkit.com/png/detail/94-948126_spider-man-flying-spiderman-png.png";
 
-  // if(play){
-  //   $('.play').css('display','block')
-  // }
-
+   if(play){
+     $('.play').css('display','block')
+   }else{
+    $('.play').css('display','none')
+   }
+useEffect(()=>{
+fetch("https://peaceful-forest-62260.herokuapp.com/")
+.then(res => res.json())
+.then(data => {
+  console.log(data.moviesData)
+})
+},[])
   return (
     <div className="card" style={{ backgroundImage: `url(${image})` }}>
       <div className="inner-card">
@@ -21,7 +29,7 @@ const Trailer = () => {
           <h6>55555 votes</h6>
         </div>
       </div>
-      <button className="btn">
+      <button className="btn" onClick={()=>setPlay(true)} onDoubleClick={()=>setPlay(false)}>
         <i className="fa fa-play"></i>
       </button>
       <p className="movieName">Movie Name</p>
