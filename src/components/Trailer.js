2cc1784da342787ae393b2887540529ded8c0423
ARMAN ALI <31398598+a-mal1k@users.noreply.github.com>
2021-11-13 09:49:44 +0530

    added some styling

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -3,23 +3,29 @@ import { useState } from "react";
 const Trailer = (props) => {
   const [play, setPlay] = useState(false);
 
-   if(play){
-     $('.play').css('display','block')
-   }else{
-    $('.play').css('display','none')
-   }
+  if (play) {
+    $(".play").css("display", "block");
+  } else {
+    $(".play").css("display", "none");
+  }
   return (
     <div className="card" style={{ backgroundImage: `url(${props.image})` }}>
       <div className="inner-card">
         <p className="date">{props.date}</p>
+
         <div className="voting">
           <span>
-            <i className="fa fa-thumbs-up"></i>{props.votePer}
+            <i className="fa fa-thumbs-up"></i>
+            {props.votePer}
           </span>
           <h6>{props.vote} votes</h6>
         </div>
       </div>
-      <button className="btn" onClick={()=>setPlay(true)} onDoubleClick={()=>setPlay(false)}>
+      <button
+        className="btn"
+        onClick={() => setPlay(true)}
+        onDoubleClick={() => setPlay(false)}
+      >
         <i className="fa fa-play"></i>
       </button>
       <p className="movieName">{props.movieName}</p>
