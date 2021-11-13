6416a50af8a18da2a8cfc0dd0126e02cc11a0f95
ARMAN ALI <31398598+a-mal1k@users.noreply.github.com>
2021-11-13 08:56:32 +0530

    fetched and displayed trailers api

--- src/components/Trailers.js
+++ src/components/Trailers.js
@@ -1,43 +1,34 @@
 import { useEffect, useState } from "react";
 import Trailer from "./Trailer";
 const Trailers = () => {
-  var apiData = [
-    { date: "" },
-    { image: "" },
-    { vote: "" },
-    { votePer: "" },
-    { movieName: "" }
-  ];
-  const fetchData = () => {
-    fetch("https://peaceful-forest-62260.herokuapp.com/")
-      .then((res) => res.json())
-      .then((data) => {
-        Object.values(data.moviesData).map((t) => {
-          apiData.date.push(t.showDate);
-          apiData.image.push(t.EventImageUrl);
-          apiData.vote.push(t.csCount);
-          apiData.votePer.push(t.wtsPerc);
-          apiData.movieName.push(t.EventTitle);
-          return apiData;
-        });
-      });
-  };
+  const [data,setData]= useState([])
+
+
+  async function fetchData(){
+    let arr = []
+    await   fetch("https://peaceful-forest-62260.herokuapp.com/")
+    .then((res) => res.json())
+    .then((data) => {
+      Object.values(data.moviesData).map((t) =>arr.push(t));
+    });
+    setData(arr);
+  }
   useEffect(() => {
-    fetchData();
+  fetchData();
   }, []);
+ data.map(t => console.log(t.ShowDate))
   return (
     <div className="trailers">
-      {apiData.map((data) => {
-        return (
+      {data.map((data) => 
           <Trailer
-            date={data.date}
-            image={data.image}
-            vote={data.vote}
-            votePer={data.votePer}
-            movieName={data.movieName}
+          key={data.EventGroup}
+            date={data.ShowDate}
+            image={data.EventImageUrl}
+            vote={data.csCount}
+            votePer={data.wtsPerc}
+            movieName={data.EventTitle}
           />
-        );
-      })}
+      )}
     </div>
   );
 };
